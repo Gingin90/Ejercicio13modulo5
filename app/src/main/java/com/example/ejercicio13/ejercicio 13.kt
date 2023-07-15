132cb996a15ea0f198cf63c2cf9523c5b5d65900
ginger cea <gingercea@gmail.com>
2023-07-14 21:21:05 -0400

    Ejercicio 13 A finalizado

--- app/src/main/java/com/example/ejercicio13/ejercicio 13.kt
+++ app/src/main/java/com/example/ejercicio13/ejercicio 13.kt
@@ -1,5 +1,7 @@
 package com.example.ejercicio13
 
+import java.text.NumberFormat
+import java.util.Currency
 import kotlin.math.ceil
 
 
@@ -8,12 +10,23 @@ fun main(){
     println(resultado )
     var redondeo = redondeo(param1 = 7.1)
     println(redondeo)
-}
+    var  muestraResultado = muestraResultado(8.57)
+    println(muestraResultado)
+
+    }
+
 
 fun suma (param1:Double,param2:Float):Double=  param1 + param2
 
 fun redondeo (param1: Double)= ceil(param1)
 
+fun muestraResultado(numero: Double) : String {
 
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.setMaximumFractionDigits(2)
+    format.setCurrency(Currency.getInstance("EUR"))
+    println(format.format(numero))
+    return format.format(numero)
+}
 
 
